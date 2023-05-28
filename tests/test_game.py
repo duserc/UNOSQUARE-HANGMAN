b91d5d0fa37744a28c2af151e5663a8bfc3e2a9b
duserc <danielcollison@hotmail.co.uk>
2023-05-28 06:50:40 +0100

    added incorrect word test

--- tests/test_game.py
+++ tests/test_game.py
@@ -202,7 +202,118 @@ class TestGameController(unittest.TestCase):
                 response = client.post(f'/games/{id}/guesses', json={"letter": "#"})
                 self.assertEqual(response.status_code, 400)
                 expected_response = {'Message': 'Guess must be supplied with 1, letter'}
-                self.assertEqual(response.get_json(), expected_response)                
+                self.assertEqual(response.get_json(), expected_response)
+                
+    @patch('controllers.game.generate_word', mock_generate_word)
+    @patch('uuid.uuid4', mock_uuid)
+    def test_make_guess_correct_word(self):
+        id, code = start_game()
+        self.assertEqual(code, 201)
+        self.assertEqual(id, GAMEID)
+        with app.app_context():
+            response = get_game_state(id)
+            self.assertEqual(response.status_code, 200)
+            word = mock_generate_word()
+            self.assertEqual(word, "Banana") 
+            expected_json = {
+                "guesses_so_far": [],
+                "remaining_attempts": 6,
+                "status": "waiting first guess",
+                "word": "______"
+            }
+            self.assertEqual(response.json, expected_json)
+            with app.test_client() as client:
+                response = client.post(f'/games/{id}/guesses', json={"letter": "B"})
+                self.assertEqual(response.status_code, 200)
+                expected_response = {
+                "guesses_so_far": ["b"],
+                "remaining_attempts": 6,
+                "status": "in progress",
+                "word": "B_____"
+                }
+                self.assertEqual(response.get_json(), expected_response) 
+                response = client.post(f'/games/{id}/guesses', json={"letter": "a"})
+                self.assertEqual(response.status_code, 200)
+                expected_response = {
+                "guesses_so_far": ["b", "a"],
+                "remaining_attempts": 6,
+                "status": "in progress",
+                "word": "Ba_a_a"
+                }
+                self.assertEqual(response.get_json(), expected_response)
+                response = client.post(f'/games/{id}/guesses', json={"letter": "n"})
+                self.assertEqual(response.status_code, 200)
+                expected_response = {"Message": "Congratulations! You have guessed the word correctly."}
+                self.assertEqual(response.get_json(), expected_response)
+            
+    @patch('controllers.game.generate_word', mock_generate_word)
+    @patch('uuid.uuid4', mock_uuid)
+    def test_make_guess_incorrect_word(self):
+        id, code = start_game()
+        self.assertEqual(code, 201)
+        self.assertEqual(id, GAMEID)
+        with app.app_context():
+            response = get_game_state(id)
+            self.assertEqual(response.status_code, 200)
+            word = mock_generate_word()
+            self.assertEqual(word, "Banana") 
+            expected_json = {
+                "guesses_so_far": [],
+                "remaining_attempts": 6,
+                "status": "waiting first guess",
+                "word": "______"
+            }
+            self.assertEqual(response.json, expected_json)
+            with app.test_client() as client:
+                response = client.post(f'/games/{id}/guesses', json={"letter": "x"})
+                self.assertEqual(response.status_code, 200)
+                expected_response = {
+                "guesses_so_far": ["x"],
+                "remaining_attempts": 5,
+                "status": "in progress",
+                "word": "______"
+                }
+                self.assertEqual(response.get_json(), expected_response) 
+                response = client.post(f'/games/{id}/guesses', json={"letter": "y"})
+                self.assertEqual(response.status_code, 200)
+                expected_response = {
+                "guesses_so_far": ["x", "y"],
+                "remaining_attempts": 4,
+                "status": "in progress",
+                "word": "______"
+                }
+                self.assertEqual(response.get_json(), expected_response) 
+                response = client.post(f'/games/{id}/guesses', json={"letter": "z"})
+                self.assertEqual(response.status_code, 200)
+                expected_response = {
+                "guesses_so_far": ["x", "y", "z"],
+                "remaining_attempts": 3,
+                "status": "in progress",
+                "word": "______"
+                }
+                self.assertEqual(response.get_json(), expected_response) 
+                response = client.post(f'/games/{id}/guesses', json={"letter": "e"})
+                self.assertEqual(response.status_code, 200)
+                expected_response = {
+                "guesses_so_far": ["x", "y", "z", "e"],
+                "remaining_attempts": 2,
+                "status": "in progress",
+                "word": "______"
+                }
+                self.assertEqual(response.get_json(), expected_response) 
+                response = client.post(f'/games/{id}/guesses', json={"letter": "f"})
+                self.assertEqual(response.status_code, 200)
+                expected_response = {
+                "guesses_so_far": ["x", "y", "z", "e", "f"],
+                "remaining_attempts": 1,
+                "status": "in progress",
+                "word": "______"
+                }
+                self.assertEqual(response.get_json(), expected_response) 
+                response = client.post(f'/games/{id}/guesses', json={"letter": "g"})
+                self.assertEqual(response.status_code, 422)
+                expected_response = {"Error": "No more attempts left, game over"}
+                 
 
 if __name__ == '__main__':
     unittest.main()
